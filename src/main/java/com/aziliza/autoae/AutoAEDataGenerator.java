27a06efceb98f6cbaa726a54339a31df41bbb7c5
虔屿 <qianyu@koubei.com>
2025-07-05 19:59:27 +0800

    first commit

---
+++ src/main/java/com/aziliza/autoae/AutoAEDataGenerator.java
@@ -0,0 +1,11 @@
+package com.aziliza.autoae;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AutoAEDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
