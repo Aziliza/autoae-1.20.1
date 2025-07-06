fe125ae67ec5dd872e8bf0ae0d91998b994b9ec0
reacher <626269672@qq.com>
2025-07-06 11:46:05 +0800

    修复了Client没写

---
+++ src/main/java/com/aziliza/autoae/AutoAEClient.java
@@ -0,0 +1,15 @@
+/*
+ * @Author: qianyu
+ * @Date: 2025-07-06 11:36:31
+ * @LastEditTime: 2025-07-06 11:38:45
+ */
+package com.aziliza.autoae;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AutoAEClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
